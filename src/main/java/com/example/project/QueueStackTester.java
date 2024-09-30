2d6f133abc37b4f1d7d396d1e0895f9f1445661d
khalidAlfaleh <444102210@student.ksu.edu.sa>
2024-09-30 14:32:59 +0300

    Update QueueStackTester.java

--- src/main/java/com/example/project/QueueStackTester.java
+++ src/main/java/com/example/project/QueueStackTester.java
@@ -31,10 +31,10 @@ public class QueueStackTester {
     }
     public static <T> void remove(LinkedPQ<T> pq, int p)
     {
-         PQNode<T> x;
+         PQElement<T> x;
         int i = pq.leangth;
         while( i > 0){
-        x = new PQNode<T>(pq.serve());
+        x = pq.serve();
             if(!(x.priority == p))
             pq.enqueue(x);
 
